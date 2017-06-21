99a3ef0624a0eadb540a3eaae8acfc293fc683d4
Enrico Ambrosini <ea@fabtotum.com>
2017-06-21 15:16:01 +0200

    Revisioned profiles, added quality files generator, tested with Cura 2.6

---
+++ Cura2.x_configs_FABtotum/cfg_writer.py
@@ -0,0 +1,87 @@
+import ConfigParser
+
+qualities = {
+    "fast": {
+        ("general", "name"): "Fast Quality",
+        ("metadata", "weight"): "-1",
+        "layer_height": "0.2",
+        "layer_height_0": "0.2",
+        "cool_fan_full_at_height": "0.4",
+    },
+        
+    "normal": {
+        ("general", "name"): "Normal Quality",
+        ("metadata", "weight"): "0",
+        "layer_height": "0.15",
+        "layer_height_0": "0.15",
+        "cool_fan_full_at_height": "0.3",
+    },
+
+    "high": {
+        ("general", "name"): "High Quality",
+        ("metadata", "weight"): "1",
+        "layer_height": "0.1",
+        "layer_height_0": "0.1",
+        "cool_fan_full_at_height": "0.2",
+    }
+}
+
+attributes = {
+    "pla": {
+        "adhesion_type": "skirt",
+        "cool_fan_speed": "100",
+        "cool_fan_speed_max": "100",
+        "cool_fan_speed_min": "100",
+        "cool_min_layer_time": "5",
+    },
+    "abs": {
+        "adhesion_type": "raft",
+        "cool_fan_speed": "50",
+        "cool_fan_speed_max": "50",
+        "cool_fan_speed_min": "50",
+        "cool_min_layer_time": "3",
+    },
+    "nylon": {
+        "adhesion_type": "raft",
+        "cool_fan_speed": "65",
+        "cool_fan_speed_max": "65",
+        "cool_fan_speed_min": "65",
+        "cool_min_layer_time": "4",
+    }
+}
+
+colours = {
+    "pla": ["yellow", "white", "red", "orange", "green", "brown", "blue", "black", "magenta", "silver", "gold", "wood"],
+    "abs": ["white", "red", "black"],
+    "nylon": ["natural"]
+}
+
+class MyConfigParser(ConfigParser.SafeConfigParser):
+    def implicit_set(self, attr, val):
+        if isinstance(attr, tuple):
+            cls, attr = attr
+        else:
+            cls = "values"
+        ConfigParser.SafeConfigParser.set(self, cls, attr, val)
+
+
+for filament, colors in colours.iteritems():
+    for color in colors:
+        for quality, quality_attrs in qualities.iteritems():
+            config = MyConfigParser()
+            with open("fabtotum_conf_factory.cfg") as f:
+                config.readfp(f)
+
+            config.set("metadata", "material", "fabtotum_%s_%s" % (filament, color))
+            config.set("metadata", "quality_type", quality)
+            out_name = "fabtotum_%s_%s_%s.inst.cfg" % (filament, color, quality)
+
+            for attribute, value in quality_attrs.iteritems():
+                print "setting %s to %s" % (attribute, value)
+                config.implicit_set(attribute, value)
+            for attribute, value in attributes[filament].iteritems():
+                print "setting %s to %s" % (attribute, value)
+                config.implicit_set(attribute, value)
+
+            with open(out_name, 'wb') as config_out:
+                config.write(config_out)
